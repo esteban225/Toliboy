abe45418764bba11f661710f3823ecb02193b8b4
estebanRicardo <esteban.ricardo.dev@gmail.com>
2025-09-03 15:22:53 -0500

    correcion use ref en home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import "../App.css";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Link } from "react-router-dom";
